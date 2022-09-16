596312d7bd3f913d2028d06dbe0a2e8d4d05004c
RenaudPavie <renaud.hpavie@gmail.com>
2022-09-16 19:30:05 +0200

    setup F1 api request

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from 'react'
 
 function App() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [infoFromAPI, setInfoFromAPI] = useState([]);
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    useEffect(() => {
+      fetch("https://ergast.com/api/f1/current.json",requestOptions)
+        .then(res => res.json())
+        .then(result => {
+            setIsLoaded(true);
+            setInfoFromAPI(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        })
+    }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
