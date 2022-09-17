85c01e673abb4a98549f7849b3a385ee4b6f1ec4
RenaudPavie <renaud.hpavie@gmail.com>
2022-09-17 15:29:36 +0200

    update request to directly get next race and display local timefor the next race

--- src/App.js
+++ src/App.js
@@ -4,25 +4,13 @@ import {useEffect,useState} from 'react'
 import NextRace from './components/NextRace/NextRace'
 
 function App() {
-    const [infoFromAPI, setInfoFromAPI] = useState([]);
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
 
-    useEffect(() => {
-      fetch("https://ergast.com/api/f1/current.json",requestOptions)
-        .then(res => res.json())
-        .then(result => {
-            setInfoFromAPI(result.MRData.RaceTable);
-        })
-    }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <NextRace data={infoFromAPI} /> 
+      <NextRace /> 
     </div>
   );
 }
--- src/components/NextRace/NextRace.jsx
+++ src/components/NextRace/NextRace.jsx
@@ -1,23 +1,48 @@
 import React, { useEffect, useState } from 'react'
 import './NextRace.css'
 
-function NextRace(props) {
-    const data = props.data
-    const [raceDate, setRaceDate] = useState(null);
-    
+function NextRace() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [infoFromAPI, setInfoFromAPI] = useState([]);
+    const [localDate, setLocalDate] = useState(null);
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    const getLocalDate = (d,t) => {
+        let newLocalDate = new Date(d+"T"+t).toLocaleString("fr-FR", {timeZone: "Europe/Paris"})
+        return newLocalDate
+    }
     useEffect(() => {
-        if(data.Races !== undefined) {
-            const nextRaceToCome = data.Races.filter(x => new Date(x.date) > new Date())
-            setRaceDate(nextRaceToCome[0])
-        }
-    },[data])
+      fetch("https://ergast.com/api/f1/current/next.json",requestOptions)
+        .then(res => res.json())
+        .then(
+            (result) => {
+            setIsLoaded(true);
+            setInfoFromAPI(result.MRData.RaceTable.Races[0]);
+            setLocalDate(getLocalDate(result.MRData.RaceTable.Races[0].date,result.MRData.RaceTable.Races[0].time))
 
-    return (
-        <div className="nextRace">
-            <p>{raceDate && raceDate.raceName} - {raceDate && raceDate.Circuit.circuitName}</p>
-            <p>{raceDate && raceDate.date} - {raceDate && raceDate.time}</p>
-        </div>
-    )
+        },
+        (error) => {
+            setIsLoaded(true);
+            setError(error);
+        })
+    }, [])
+
+    if (error) {
+        return <div>Erreur : {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Chargement...</div>;
+    } else {
+        return (
+            <div className="nextRace">
+                {console.log(infoFromAPI)}
+                <p>{infoFromAPI && infoFromAPI.raceName} - {infoFromAPI && infoFromAPI.Circuit.circuitName}</p>
+                <p>{localDate && localDate}</p>
+            </div>
+        )
+    }
 }
 
 export default NextRace
