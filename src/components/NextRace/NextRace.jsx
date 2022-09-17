6f476673f328df07969662e6bda17a9c735ee05f
RenaudPavie <renaud.hpavie@gmail.com>
2022-09-17 23:01:34 +0200

    detect next session in coming (FP1,FP2,FP3,Qualifs,Sprint,Race)and display timeleft

--- src/components/NextRace/NextRace.jsx
+++ src/components/NextRace/NextRace.jsx
@@ -5,21 +5,74 @@ function NextRace() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [infoFromAPI, setInfoFromAPI] = useState([]);
-    const [localDate, setLocalDate] = useState(null);
+    const [localDate, setLocalDate] = useState([
+        {
+            session: '',
+            date: ''
+        }
+    ]);
     const requestOptions = {
         method: 'GET',
         redirect: 'follow'
     };
+    const getNextSession = (arr) => {
+        arr.sort((a,b)=>a.date - b.date);
+        let newArr = arr.filter(x => {
+            return x.date > new Date()
+        })
+        return newArr
+    }
     useEffect(() => {
       fetch("https://ergast.com/api/f1/current/next.json",requestOptions)
         .then(res => res.json())
         .then(
             (result) => {
-            console.log(result);
+            // console.log(result);
             setIsLoaded(true);
-            setInfoFromAPI(result.MRData.RaceTable.Races[0]);
-            let fixedDate = new Date(result.MRData.RaceTable.Races[0].date+'T'+result.MRData.RaceTable.Races[0].time)
-            setLocalDate(fixedDate)
+            const data = result.MRData.RaceTable.Races[0];
+            setInfoFromAPI(data);
+            let firstPracticeDate = new Date(data.FirstPractice.date+'T'+data.FirstPractice.time)
+            let secondPracticeDate = new Date(data.SecondPractice.date+'T'+data.SecondPractice.time)
+            let qualifyingDate = new Date(data.Qualifying.date+'T'+data.Qualifying.time)
+            let raceDate = new Date(data.date+'T'+data.time)
+            const dateArr = [
+                {
+                    session : 'FP1',
+                    date: firstPracticeDate
+                },
+                {
+                    session : 'FP2',
+                    date: secondPracticeDate
+                },
+                {
+                    session : 'Qualifs',
+                    date: qualifyingDate
+                },
+                {
+                    session : 'Course',
+                    date: raceDate
+                }
+            ]
+            if (data.hasOwnProperty('ThirdPractice')) {
+                let thirdPracticeDate = new Date(data.ThirdPractice.date+'T'+data.ThirdPractice.time)
+                dateArr.push(
+                {
+                    session : 'FP3',
+                    date: thirdPracticeDate
+                })
+            }
+            if (data.hasOwnProperty('Sprint')) {
+                let sprintRaceDate = new Date(data.Sprint.date+'T'+data.Sprint.time)  
+                dateArr.push(
+                    {
+                        session : 'Sprint',
+                        date: sprintRaceDate
+                    })
+            }
+            let filteredArray = getNextSession(dateArr)
+            console.log(filteredArray)
+            // setLocalDate(raceDate)
+            setLocalDate(filteredArray)
         },
         (error) => {
             setIsLoaded(true);
@@ -28,7 +81,7 @@ function NextRace() {
     }, [])
 
     const calculateTimeLeft = (d) => {
-        const difference = d - new Date();
+        const difference = d[0].date - new Date();
         let timeLeft = {};
 
         if (difference > 0) {
@@ -57,9 +110,9 @@ function NextRace() {
         return (
             <div className="nextRace">
                 <h2>{infoFromAPI && infoFromAPI.raceName} - {infoFromAPI && infoFromAPI.Circuit.circuitName}</h2>
-                <p>
-                    {timeLeft && `${Object.keys(timeLeft).length === 0 ? 'Calculating...' : 
-                        `J : ${timeLeft.days} H : ${timeLeft.hours} M : ${timeLeft.minutes} S : ${timeLeft.seconds}`
+                <p><strong>{localDate && localDate[0].session}</strong> - 
+                    {timeLeft && `${Object.keys(timeLeft).length === 0 ? 'En cours de calcul...' : 
+                        ` J : ${timeLeft.days} H : ${timeLeft.hours} M : ${timeLeft.minutes} S : ${timeLeft.seconds}`
                     }`} 
                 </p>
             </div>
