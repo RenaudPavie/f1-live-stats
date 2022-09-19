0ae064bc117382c19d9f9f802bd3a3fd36ba1f90
RenaudPavie <renaud.hpavie@gmail.com>
2022-09-19 18:13:49 +0200

    fetch data and create constructor ranking component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import NextRace from './components/NextRace/NextRace'
+import DriversRanking from './components/DriversRanking/DriversRanking'
+import ConstructorsRanking from './components/ConstructorsRanking/ConstructorsRanking'
 
 function App() {
 
@@ -10,6 +12,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <NextRace /> 
+      <DriversRanking /> 
+      <ConstructorsRanking /> 
     </div>
   );
 }
---
+++ src/components/ConstructorsRanking/ConstructorsRanking.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import './ConstructorsRanking.css'
+
+function ConstructorsRanking() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [infoFromAPI, setInfoFromAPI] = useState([]);
+   
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+    useEffect(() => {
+      fetch("https://ergast.com/api/f1/current/constructorStandings.json",requestOptions)
+        .then(res => res.json())
+        .then(
+            (result) => {
+            setIsLoaded(true);
+            const data = result.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+            console.log(data)
+            setInfoFromAPI(data);
+            
+        },
+        (error) => {
+            setIsLoaded(true);
+            setError(error);
+        })
+    }, [])
+
+
+
+    if (error) {
+        return <div>Erreur : {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Chargement...</div>;
+    } else {
+        return (
+            <div className="ConstructorsRanking">
+                <table>
+                    <tbody>
+                        {infoFromAPI && console.log(infoFromAPI)}
+                        {infoFromAPI && infoFromAPI.map((data,index) => {
+                            return <tr key={index}>
+                                <td>{data.position}</td>
+                                <td>{data.Constructor.name}</td>
+                                <td>{data.points}</td>
+                                </tr>
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default ConstructorsRanking
