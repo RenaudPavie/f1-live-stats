d1773fc58e503eeee13da3f2351b73413c77aa18
RenaudPavie <renaud.hpavie@gmail.com>
2022-09-19 18:08:24 +0200

    fetch data from API and create table with drivers ranking

---
+++ src/components/DriversRanking/DriversRanking.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import './DriversRanking.css'
+
+function DriversRanking() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [infoFromAPI, setInfoFromAPI] = useState([]);
+   
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+    useEffect(() => {
+      fetch("https://ergast.com/api/f1/current/driverStandings.json",requestOptions)
+        .then(res => res.json())
+        .then(
+            (result) => {
+            setIsLoaded(true);
+            const data = result.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+            // console.log(data)
+            setInfoFromAPI(data);
+            
+        },
+        (error) => {
+            setIsLoaded(true);
+            setError(error);
+        })
+    }, [])
+
+
+
+    if (error) {
+        return <div>Erreur : {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Chargement...</div>;
+    } else {
+        return (
+            <div className="DriversRanking">
+                <table>
+                    <tbody>
+                        {/* {infoFromAPI && console.log(infoFromAPI)} */}
+                        {infoFromAPI && infoFromAPI.map((data,index) => {
+                            return <tr key={index}>
+                                <td>{data.Driver.code}</td>
+                                <td>{data.Constructors[0].name}</td>
+                                <td>{data.position}</td>
+                                <td>{data.points}</td>
+                                </tr>
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default DriversRanking
